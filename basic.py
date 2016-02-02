cb0def6ac72b39d6d29d6f282ea92e9729308a69
Ionut Alin Neagu <alin@Ionuts-MacBook-Pro.local>
2016-02-02 01:07:10 +0000

    end of 4

--- basic.py
+++ basic.py
@@ -37,7 +37,7 @@ def lex(filecontents):
         elif tok == "0" or tok == "1" or tok == "2" or tok == "3" or tok == "4" or tok == "5" or tok == "6" or tok == "7" or tok == "8" or tok == "9":
             expr += tok
             tok = ""
-        elif tok == "+":
+        elif tok == "+" or tok == "-" or tok == "*" or tok == "/" or tok == "(" or tok == ")":
             isexpr = 1
             expr += tok
             tok = ""
@@ -56,13 +56,28 @@ def lex(filecontents):
     print(tokens)
     return tokens
     
+    
+def doPRINT(toPRINT):
+    if toPRINT[0:6] == "STRING":
+        toPRINT = toPRINT[8:]
+        toPRINT = toPRINT[:-1]
+    if toPRINT[0:3] == "NUM":
+        toPRINT = toPRINT[4:]
+    if toPRINT[0:4] == "EXPR":
+        toPRINT = toPRINT[5:]
+    print(toPRINT)
         
         
 def parse(toks):
     i = 0
     while(i<len(toks)):
-        if toks[i] + " " + toks[i+1][0:6] == "PRINT STRING":
-            print(toks[i+1][7:])
+        if toks[i] + " " + toks[i+1][0:6] == "PRINT STRING" or toks[i] + " " + toks[i+1][0:4] == "PRINT EXPR" or toks[i] + " " + toks[i+1][0:3] == "PRINT NUM":
+            if toks[i+1][0:6] == "STRING":
+                doPRINT(toks[i+1])
+            elif toks[i+1][0:3] == "NUM":
+                doPRINT(toks[i+1])
+            elif toks[i+1][0:4] == "EXPR":
+                doPRINT(toks[i+1])
         i += 2
         
 def run():
